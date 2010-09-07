2ae4a3fcd87e42146f14a7d1dc13f434241f5238
Tim Dysinger <tim@dysinger.net>
2010-09-06 19:29:59 -1000

    Added a lein project file

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject apparatus "0.1.0-SNAPSHOT"
+  :description "Apparatus: Clojure Clusters"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [com.hazelcast/hazelcast "1.9-RC"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :namespaces [apparatus.config
+               apparatus.eval
+               apparatus.cluster])
