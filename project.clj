0e3bcc4617894ae3933cbbdd353fc8bb0a920b5b
Tim Dysinger <tim@dysinger.net>
2010-10-18 15:16:16 -0700

    Updated to hazelcast 1.9

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject apparatus "0.1.0-SNAPSHOT"
   :description "Apparatus: Clojure Cluster"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [com.hazelcast/hazelcast "1.9-RC"]]
+                 [com.hazelcast/hazelcast "1.9"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :namespaces [apparatus.eval
                apparatus.main]
