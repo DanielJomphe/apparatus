235841e0ba38581ede023e120793df46e07f82a1
Tim Dysinger <tim@dysinger.net>
2010-10-23 11:02:12 -0400

    Reconfigured AOT

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.hazelcast/hazelcast "1.9"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
-  :namespaces [apparatus.eval
-               apparatus.main]
+  :namespaces [apparatus.eval]
   :main apparatus.main)
