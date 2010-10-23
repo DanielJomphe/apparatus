a9d46012481c90ca869400df65575f02468746a4
Tim Dysinger <tim@dysinger.net>
2010-10-23 11:28:03 -0400

    Added a demo namespace

---
+++ src/apparatus/demo.clj
@@ -0,0 +1,27 @@
+(ns apparatus.demo
+  (:require [apparatus.config :as config]
+            [apparatus.cluster :as cluster])
+  (:import [java.util UUID]))
+
+(defn hello-world []
+  (cluster/eval-any
+   '(do (println 'hello)
+        (use 'clojure.contrib.shell)
+        (sh "say" "hello world!"))))
+
+(defn peanut-butter-jelly-time! []
+  (cluster/eval-every
+   '(do (println 'peanutbutterjellytime)
+        (use 'clojure.contrib.shell)
+        (sh "say" "it's peanut-butter-jelly time!"))
+   (cluster/members)))
+
+(defn distributed-data-locality-demo []
+  (doseq [x (range 256)]
+    (-> (cluster/map "demo")
+        (.put (str x) (str (UUID/randomUUID)))))
+  (doseq [x (range 256)]
+    (.get (cluster/eval
+           `(do (require '[apparatus.cluster :as cluster])
+                (-> (cluster/map "demo") (.get (str ~x)) println))
+           (str x)))))
