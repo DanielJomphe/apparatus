a81b42d5d39ae7f380c29f36a040299cc47624d0
Tim Dysinger <tim@dysinger.net>
2010-10-24 00:55:47 -0400

    Removed extra dep

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject apparatus "0.1.0-SNAPSHOT"
   :description "Apparatus: Clojure Cluster"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [com.hazelcast/hazelcast "1.9"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :namespaces [apparatus.eval]
