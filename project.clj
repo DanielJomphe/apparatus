ec66f3ea7fdf1723f28488b9f389b5b8c85bb05c
Tim Dysinger <tim@dysinger.net>
2011-01-11 08:02:53 -0800

    Upgraded lein to 1.4.2

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.hazelcast/hazelcast "1.9"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
-  :namespaces [apparatus.eval]
+  :aot [apparatus.eval]
   :main apparatus.demo)
