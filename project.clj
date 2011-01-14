c6a580aff062d9c306ade59cd8225add502588e6
Tim Dysinger <tim@dysinger.net>
2011-01-13 19:33:56 -0500

    Updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject apparatus "0.1.4"
+(defproject apparatus "0.2.0-SNAPSHOT"
   :description "Apparatus: Clojure Cluster"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.hazelcast/hazelcast "1.9.1"]]
