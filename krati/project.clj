64b70f9ea74ccfa5511237be12220d0aed1ee77e
Tim Dysinger <tim@dysinger.net>
2011-01-15 22:41:53 -0500

    Updated project.clj with better deps

--- krati/project.clj
+++ krati/project.clj
@@ -1,6 +1,5 @@
 (defproject apparatus-krati "0.2.0-SNAPSHOT"
   :description "Apparatus Krati: Fast K/V Disk Persistence"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [com.hazelcast/hazelcast "1.9.1"]
-                 [org.linkedin/krati "0.3.7"]]
-  :dev-dependencies [[apparatus "0.2.0-SNAPSHOT"]])
+                 [apparatus "0.2.0-SNAPSHOT"]
+                 [org.linkedin/krati "0.3.7"]])
