dbd617ff233ec1dbf0cf2de13119deacd0d24051
Tim Dysinger <tim@dysinger.net>
2011-01-19 21:05:36 -1000

    Changed description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject apparatus "0.2.0-SNAPSHOT"
-  :description "Apparatus: Clojure Cluster"
+  :description "Apparatus is a Clojure Cluster"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.hazelcast/hazelcast "1.9.1"]]
   :aot [apparatus.eval]
