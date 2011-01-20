d1d555b97e5e3dd00bf482aacf6078e70e1ab6d0
Tim Dysinger <tim@dysinger.net>
2011-01-19 21:06:44 -1000

    Added pallet

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject apparatus "0.2.0-SNAPSHOT"
   :description "Apparatus is a Clojure Cluster"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [com.hazelcast/hazelcast "1.9.1"]]
-  :aot [apparatus.eval]
+                 [com.hazelcast/hazelcast "1.9.1"]
                  [org.linkedin/krati "0.3.7"]
+                 [org.jclouds/jclouds-all "1.0-beta-8"]
+                 [org.cloudhoist/pallet "0.4.0-SNAPSHOT"]
+                 [org.cloudhoist/automated-admin-user "0.4.0-SNAPSHOT"]
+                 [org.cloudhoist/java "0.4.0-SNAPSHOT"]]
+  :aot [apparatus.eval])
