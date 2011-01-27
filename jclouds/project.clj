1d4a4ad89233ec2d5f51b6dd9de5fb7b0d96e6bb
Tim Dysinger <tim@dysinger.net>
2011-01-26 17:53:49 -1000

    moved jclouds

---
+++ jclouds/project.clj
@@ -0,0 +1,4 @@
+(defproject apparatus-jclouds "1.0.0-SNAPSHOT"
+  :description "Apparatus: JClouds Discovery"
+  :dependencies [[apparatus "1.0.0-SNAPSHOT"]
+                 [org.jclouds/jclouds-all "1.0-SNAPSHOT"]])
