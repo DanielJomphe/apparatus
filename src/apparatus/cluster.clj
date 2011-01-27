5d6ae88b603355072eb5d4334540aa13c1f98230
Tim Dysinger <tim@dysinger.net>
2011-01-26 17:55:41 -1000

    Updated to Apache 2.0 LICENSE

--- src/apparatus/cluster.clj
+++ src/apparatus/cluster.clj
@@ -1,3 +1,17 @@
+;; Copyright 2010-2011 Tim Dysinger <tim@dysinger.net>
+
+;; Licensed under the Apache License, Version 2.0 (the "License"); you
+;; may not use this file except in compliance with the License.  You
+;; may obtain a copy of the License at
+
+;; http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+;; implied.  See the License for the specific language governing
+;; permissions and limitations under the License.
+
 (ns apparatus.cluster
   (:import [com.hazelcast.core Hazelcast DistributedTask MultiTask]
            [apparatus Eval]))
@@ -39,6 +53,18 @@
     (-> (Hazelcast/getExecutorService)
         (.execute task))
     task))
+
+(defmacro with-tx
+  "FIXME needs better docs"
+  [& body]
+  `(let [tx# (Hazelcast/getTransaction)]
+     (try
+       (.begin tx#)
+       ~@body
+       (.commit tx#)
+       (catch Exception e#
+         (.rollback tx#)
+         (throw e#)))))
 
 (defn get-topic
   "Returns a distributed topic for the given key."
--- src/apparatus/config.clj
+++ src/apparatus/config.clj
@@ -1,3 +1,17 @@
+;; Copyright 2010-2011 Tim Dysinger <tim@dysinger.net>
+
+;; Licensed under the Apache License, Version 2.0 (the "License"); you
+;; may not use this file except in compliance with the License.  You
+;; may obtain a copy of the License at
+
+;; http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+;; implied.  See the License for the specific language governing
+;; permissions and limitations under the License.
+
 (ns apparatus.config
   (:import [com.hazelcast.config Config GroupConfig]))
 
@@ -5,6 +19,11 @@
   "Produce a default multicast configuration"
   []
   (Config.))
+
+(defn config-slurp
+  [filename]
+  ;; TODO slurp a file in - note hazelcast already has an XML file format
+  )
 
 (defn config-group
   "Specify a specific name & password to avoid collisions with other clusters"
