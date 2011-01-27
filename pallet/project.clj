61d3668f808bb5225b7857783da76b4249b2eaa4
Tim Dysinger <tim@dysinger.net>
2011-01-26 22:02:49 -1000

    Added more deps

--- pallet/project.clj
+++ pallet/project.clj
@@ -2,4 +2,6 @@
   :description "Apparatus: Pallet Deployment"
   :dependencies [[org.cloudhoist/pallet "0.4.0-SNAPSHOT"]
                  [org.cloudhoist/automated-admin-user "0.4.0-SNAPSHOT"]
-                 [org.cloudhoist/java "0.4.0-SNAPSHOT"]])
+                 [org.cloudhoist/java "0.4.0-SNAPSHOT"]
+                 [org.jclouds/jclouds-allcompute "1.0-beta-8"]]
+  :dev-dependencies [[org.cloudhoist/pallet-lein "0.3.0-SNAPSHOT"]])
