11e69035daeca423d76e9eded76ba1353de5063b
Tim Dysinger <tim@dysinger.net>
2011-01-26 17:54:29 -1000

    moved krati

---
+++ krati/project.clj
@@ -0,0 +1,4 @@
+(defproject apparatus-krati "1.0.0-SNAPSHOT"
+  :description "Apparatus: Krati Disk Store"
+  :dependencies [[apparatus "1.0.0-SNAPSHOT"]
+                 [org.linkedin/krati "0.3.7"]])
