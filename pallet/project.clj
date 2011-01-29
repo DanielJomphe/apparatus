d52d78a92e6987a2eb705eaf555513370233ee66
Tim Dysinger <tim@dysinger.net>
2011-01-29 13:36:00 -0800

    Added monit dep

--- pallet/project.clj
+++ pallet/project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.cloudhoist/pallet "0.4.0-SNAPSHOT"]
                  [org.cloudhoist/automated-admin-user "0.4.0-SNAPSHOT"]
                  [org.cloudhoist/java "0.4.0-SNAPSHOT"]
+                 [org.cloudhoist/monit "0.4.0-SNAPSHOT"]
                  [org.jclouds/jclouds-allcompute "1.0-beta-8"]]
   :dev-dependencies [[org.cloudhoist/pallet-lein "0.3.0-SNAPSHOT"]])
