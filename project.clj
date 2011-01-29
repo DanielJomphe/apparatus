33881762dbcf2be8d42ef78a33d61533b38f41b7
Tim Dysinger <tim@dysinger.net>
2011-01-29 13:35:37 -0800

    Added logging dep

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject apparatus "1.0.0-SNAPSHOT"
   :description "Apparatus: Clojure Clusters"
   :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure.contrib/logging "1.3.0-SNAPSHOT"]
                  [com.hazelcast/hazelcast "1.9.1"]
                  [commons-daemon "1.0.3"]]
   :aot [apparatus.eval]
