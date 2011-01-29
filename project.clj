3459099e228c9cba61de2c0657cbf02b43d8713a
Tim Dysinger <tim@dysinger.net>
2011-01-29 11:54:29 -0800

    Removed deps that were moved to another package

--- project.clj
+++ project.clj
@@ -1,11 +1,7 @@
-(defproject apparatus "0.2.0-SNAPSHOT"
+(defproject apparatus "1.0.0-SNAPSHOT"
   :description "Apparatus: Clojure Clusters"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.hazelcast/hazelcast "1.9.1"]
-                 [org.linkedin/krati "0.3.7"]
-                 [org.jclouds/jclouds-all "1.0-beta-8"]
-                 [org.cloudhoist/pallet "0.4.0-SNAPSHOT"]
-                 [org.cloudhoist/automated-admin-user "0.4.0-SNAPSHOT"]
-                 [org.cloudhoist/java "0.4.0-SNAPSHOT"]]
+                 [commons-daemon "1.0.3"]]
   :aot [apparatus.eval]
   :main apparatus.main)
