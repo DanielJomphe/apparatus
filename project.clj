50529b47ddbfa0a1cb01e7a995fa59cec6cacc34
Tim Dysinger <tim@dysinger.net>
2011-05-07 05:20:09 +0800

    Fix broken project.clj :( git-magit hunk-staging fail

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject apparatus "1.0.0-SNAPSHOT"
   :description "Apparatus: Clojure Clusters"
-                 [commons-daemon "1.0.3"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.hazelcast/hazelcast "1.9.2.2"]
+                 [commons-daemon "1.0.3"]]
   :aot [apparatus.eval]
   :main apparatus.main)
