1e7967112a4b36bd6abd5a5bd5b4a9b09ea6384a
Tim Dysinger <tim@dysinger.net>
2011-05-07 05:17:52 +0800

    Use the new official mvn repo files for krati

--- krati/project.clj
+++ krati/project.clj
@@ -1,4 +1,4 @@
 (defproject apparatus-krati "1.0.0-SNAPSHOT"
   :description "Apparatus: Krati Disk Store"
   :dependencies [[apparatus "1.0.0-SNAPSHOT"]
-                 [org.linkedin/krati "0.3.7"]])
+                 [com.sna-projects.krati/krati "0.3.8"]])
