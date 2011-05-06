bc550c192e18f3701bd69cde0ed300b95ee7e16c
Tim Dysinger <tim@dysinger.net>
2011-05-07 05:18:42 +0800

    Update to the latest stable hazelcast

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject apparatus "1.0.0-SNAPSHOT"
   :description "Apparatus: Clojure Clusters"
-                 [com.hazelcast/hazelcast "1.9.1"]
                  [commons-daemon "1.0.3"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [com.hazelcast/hazelcast "1.9.2.2"]
   :aot [apparatus.eval]
   :main apparatus.main)
