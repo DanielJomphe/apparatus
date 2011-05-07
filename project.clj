cc541c74b450a17f79b1a2b4881fa62f361ff3ce
Tim Dysinger <tim@dysinger.net>
2011-05-07 09:33:07 +0800

    Updated common-daemon

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Apparatus: Clojure Clusters"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.hazelcast/hazelcast "1.9.3"]
-                 [commons-daemon "1.0.3"]]
+                 [commons-daemon "1.0.5"]]
   :aot [apparatus.eval]
   :main apparatus.main)
