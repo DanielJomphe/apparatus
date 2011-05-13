b89f9af253edc10c200fdaaf6594d5db96286562
Tim Dysinger <tim@dysinger.net>
2011-05-13 17:50:14 +0800

    Only need compute for pallet deploy

--- pallet/project.clj
+++ pallet/project.clj
@@ -3,7 +3,7 @@
   :repositories {"sonatype-snapshots"
                  {:url "http://oss.sonatype.org/content/repositories/snapshots"
                   :snapshots true}}
-  :dependencies [[org.jclouds/jclouds-all "1.0-beta-9c"]
+  :dependencies [[org.jclouds/jclouds-allcompute "1.0-beta-9c"]
                  [org.cloudhoist/pallet "0.5.1-SNAPSHOT"]
                  [org.cloudhoist/automated-admin-user "0.5.1-SNAPSHOT"]
                  [org.cloudhoist/java "0.5.1-SNAPSHOT"]]
